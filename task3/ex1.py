4ad9fb5c8c3088f7a69202c2d7e049172794e60b
IrmA-ErroR <sveta.cat.kot@gmail.com>
2024-10-01 17:26:37 +0300

    updated

--- task3/ex1.py
+++ task3/ex1.py
@@ -1,11 +1,38 @@
 # 1.	Вывести информацию в консоль о логических дисках, именах, метке тома, 
 # размере и типе файловой системы (модуль psutil).
+
 import psutil
 
-disks = psutil.disk_partitions()
 
-# информация о каждом диске
-for sdiskpart in disks:
-    print(sdiskpart)
-    break
+def disks_information():   
+    disks = psutil.disk_partitions() 
+    disk_info = {}
 
+    for disk in disks:
+
+        try:
+            usage = psutil.disk_usage(disk.mountpoint)
+            disk_info[disk.device] = {
+                    'mountpoint': disk.mountpoint,
+                    'total': usage.total / (1024 ** 3),  # Переводим в гигабайты
+                    'fstype': disk.fstype
+                }
+
+        except PermissionError:
+            disk_info[disk.device] = 'Permission denied'
+
+    return disk_info
+
+
+disks_data = disks_information()
+
+for disk_name, info in disks_data.items():
+    if isinstance(info, dict):
+        print(f"Имя: {disk_name}")
+        print(f"Метка тома: {info['mountpoint']}")
+        print(f"Размер диска: {info['total']:.2f} GB")
+        print(f"Тип файловой системы: {info['fstype']}")
+    else:
+        print(f"Имя: {disk_name}")
+        print(info)  
+    print("-" * 50)
---
+++ task3/ex2.py
@@ -0,0 +1,40 @@
+# 2. Работа с файлами (модуль os, shutil)
+# * Создать файл
+# * Записать в файл строку, введённую пользователем
+# * Прочитать файл в консоль
+# * Удалить файл
+
+import os
+import shutil
+
+def create_file(file_name, content):
+    with open(file_name, 'w') as file:
+        file.write(content)
+        print(f"Файл {file_name} успешно создан")
+
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+    return content
+
+def delete_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print(f"Файл {file_name} успешно удален")
+    else:
+        print(f"Файл {file_name} не существует")
+
+
+
+file_name = "test.txt"
+
+user_input = input("Введите строку для записи в файл: ")
+
+create_file(file_name, user_input)
+
+print(f"Строка записана в файл {file_name}")
+
+content = read_file(file_name)
+print(f"Содержимое файла:\n\t{content}")
+
+delete_file(file_name)
