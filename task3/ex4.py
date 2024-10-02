5c1c42a6f9afab3ab9963050009016125799d037
IrmA-ErroR <sveta.cat.kot@gmail.com>
2024-10-02 21:10:24 +0300

    Updated, not ready

--- task3/ex4.py
+++ task3/ex4.py
@@ -3,3 +3,64 @@
 # * Записать в файл новые данные из консоли.
 # * Прочитать файл в консоль.
 # * Удалить файл.
+
+import xml.etree.ElementTree as ET
+import os
+import re
+
+
+def create_xml_file(file_name, root_element):
+    tree = ET.ElementTree(root_element)
+    tree.write(file_name)
+    print(f"Файл {file_name} создан и данные записаны.")
+
+def read_xml_file(file_name):
+    if os.path.exists(file_name):
+        tree = ET.parse(file_name)
+        root = tree.getroot()
+        print(f"Содержимое файла {file_name}:")
+        for elem in root:
+            print(f"{elem.tag}:")
+            for sub_elem in elem:
+                print(f"  {sub_elem.tag}: {sub_elem.text}")
+    else:
+        print(f"Файл {file_name} не существует")
+
+
+def delete_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print(f"Файл {file_name} успешно удален")
+    else:
+        print(f"Файл {file_name} не существует")
+
+
+def main():
+    # user_file_name = input("Введите имя файла: ")
+    # file_name = re.sub(r'[^a-zA-Z0-9_-]', '', user_file_name) + ".xml"
+    file_name = "test.xml"
+    root = ET.Element("users_database")
+
+    while True:
+        user_name = input("Введите имя пользователя (или 'exit' для завершения): ")
+        if user_name.lower() == 'exit':
+            break
+        user_age = input("Введите возраст: ")
+
+        # Добавляем данные каждого пользователя как подэлемент <user>
+        user_element = ET.SubElement(root, 'user')        
+        name_element = ET.SubElement(user_element, 'name')
+        name_element.text = user_name
+        age_element = ET.SubElement(user_element, 'age')
+        age_element.text = user_age
+
+    create_xml_file(file_name, root)
+
+    read_xml_file(file_name)
+
+    delete_file(file_name)
+
+
+if __name__ == "__main__":
+    main()
+        
--- task3/ex5.py
+++ task3/ex5.py
@@ -3,3 +3,55 @@
 # * Добавить файл, выбранный пользователем, в архив
 # * Разархивировать файл и вывести данные о нем
 # * Удалить файл и архив
+
+import json
+import xml.etree.ElementTree as ET
+import os
+import zipfile
+
+from ex2 import create_file, read_file, delete_file
+
+def create_zip_archive(zip_name, files):
+    with zipfile.ZipFile(zip_name, 'w') as zipf:
+        for file in files:
+            zipf.write(file)
+    print(f"Архив {zip_name} успешно создан")
+
+def extract_zip_archive(zip_name, extract_path):
+    with zipfile.ZipFile(zip_name, 'r') as zipf:
+        zipf.extractall(extract_path)
+    print(f"Архив {zip_name} успешно извлечен")
+
+def get_zip_size(zip_name):
+    size = os.path.getsize(zip_name)
+    print(f"Размер архива {zip_name}: {size} байт")
+
+# Генерация данных
+def generate_user_data():
+    file_name = "test.txt"
+    user_input = input("Введите строку для записи в файл: ")
+    create_file(file_name, user_input)
+    return file_name
+
+def main():
+    zip_name = "user_data.zip"
+    test_file = generate_user_data()
+
+    create_zip_archive(zip_name, [test_file])
+
+    # Раземер архива
+    get_zip_size(zip_name)
+
+    extract_path = "."
+    os.makedirs(extract_path, exist_ok=True)
+    extract_zip_archive(zip_name, extract_path)
+
+    print("\nСодержимое извлеченного файла:")
+    extracted_data = os.path.join(extract_path, test_file)
+    print("\t", read_file(extracted_data))
+
+    delete_file(test_file)
+    delete_file(zip_name)
+
+if __name__ == "__main__":
+    main()
