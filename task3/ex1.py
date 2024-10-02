bfd9dfff40f8f33e8f2dbe91593ecd1239945cfc
IrmA-ErroR <sveta.cat.kot@gmail.com>
2024-10-02 20:48:02 +0300

    ready

--- task3/ex1.py
+++ task3/ex1.py
@@ -24,15 +24,20 @@ def disks_information():
     return disk_info
 
 
-disks_data = disks_information()
+def main():
+    disks_data = disks_information()
 
-for disk_name, info in disks_data.items():
-    if isinstance(info, dict):
-        print(f"Имя: {disk_name}")
-        print(f"Метка тома: {info['mountpoint']}")
-        print(f"Размер диска: {info['total']:.2f} GB")
-        print(f"Тип файловой системы: {info['fstype']}")
-    else:
-        print(f"Имя: {disk_name}")
-        print(info)  
-    print("-" * 50)
+    for disk_name, info in disks_data.items():
+        if isinstance(info, dict):
+            print(f"Имя: {disk_name}")
+            print(f"Метка тома: {info['mountpoint']}")
+            print(f"Размер диска: {info['total']:.2f} GB")
+            print(f"Тип файловой системы: {info['fstype']}")
+        else:
+            print(f"Имя: {disk_name}")
+            print(info)  
+        print("-" * 50)
+
+
+if __name__ == "__main__":
+    main()
